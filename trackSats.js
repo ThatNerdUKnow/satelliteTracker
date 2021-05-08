3190623656477c2ced0f9e9a53b21776361d0d80
withenex <brandonpi265@gmail.com>
2021-05-08 18:50:10 -0500

    use webpack and move code to new file

---
+++ trackSats.js
@@ -0,0 +1,34 @@
+const axios = require('axios')
+const chunk = require('lodash/chunk')
+const satellite = require('satellite.js')
+
+async function trackSats()
+{
+    var satData = await axios.get("https://www.celestrak.com/NORAD/elements/stations.txt");
+    satData = satData.data.split("\r\n")
+    
+    satData = chunk(satData,3)
+
+    var satRecs = new Array();
+
+    satData.forEach((sat)=>{
+
+        try
+        {
+        satRec = satellite.twoline2satrec(sat[1],sat[2]);
+
+        satRecs.push({"name": sat[0],satRec})
+        }
+        catch(err)
+        {
+            //console.log(err)
+        }
+
+        
+    })
+
+
+    return satRecs;
+}
+
+module.exports = trackSats;
