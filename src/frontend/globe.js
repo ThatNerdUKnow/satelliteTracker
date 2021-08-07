d9466afc4e93fa3fe379c396caaace204f28ab9e
withenex <brandonpi265@gmail.com>
2021-08-06 21:03:01 -0500

    Screw it

--- src/frontend/globe.js
+++ src/frontend/globe.js
@@ -4,6 +4,7 @@ window.CESIUM_BASE_URL = '/';
 
 import * as Cesium from 'cesium';
 
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2ZmEwZmViYi1jOGRhLTRhMjQtYTZiNC03ZjFmMjZjZThjNGMiLCJpZCI6NTUyMDMsImlhdCI6MTYyMDU4MTY2NX0.PAXxOTQ0YOUNCL56ZUfMpEXjfAjDtW26UgyVwXNvkLo'
 
 // Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
 const viewer = new Cesium.Viewer('cesiumContainer', {
